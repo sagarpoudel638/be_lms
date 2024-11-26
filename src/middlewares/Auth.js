1265f158e9bdee9dc2ef852acd9ecaa152c83f18
sagarpoudel638 <sagarpoudel638@gmail.com>
2024-11-26 22:04:48 +1100

    minor update

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -1,45 +1,44 @@
 export const authMiddleware = (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-    if (!token){
-        const respObj = {
-            status :false,
-            message : "Access denied. No token!"
-
-        };
-        return res.status(401).send(respObj);
+    if (!token) {
+      const respObj = {
+        status: false,
+        message: "Access denied. No token!",
+      };
+      return res.status(401).send(respObj);
     }
     try {
-        const decoded = jwt.verify(token, config.jwtSecret);
-        req.user = decoded;
-        next();
+      const decoded = jwt.verify(token, config.jwtSecret);
+      req.user = decoded;
+      next();
     } catch (err) {
-        if (err.name === "TokenExpiredError") {
-            const errObj = {
-              status: false,
-              message: "Token expired! Please log in again.",
-            };
-      
-            return res.status(401).json(errObj);
-          } else {
-            const errObj = {
-              status: false,
-              message: "Invalid Token!",
-            };
-      
-            return res.status(403).json(errObj);
-          }
+      if (err.name === "TokenExpiredError") {
+        const errObj = {
+          status: false,
+          message: "Token expired! Please log in again.",
+        };
+
+        return res.status(401).json(errObj);
+      } else {
+        const errObj = {
+          status: false,
+          message: "Invalid Token!",
+        };
+
+        return res.status(403).json(errObj);
+      }
     }
   } catch (error) {
     return res.json(error);
   }
 };
 
-export const isAdmin = (req, res, next) => {
-//   if (req.userdata.role == admin) {
+// export const isAdmin = (req, res, next) => {
+//   if (req.userdata.role == "admin") {
 //     next();
 //   } else {
 //     const error = { status: "error", message: "unauthorized action" };
 //   }
 //   return res.json(error);
-};
+// };
